1574e4a547657f359a4e19bea577a5b7d0db2bfe
TaskFlow DevOps <devops@taskflow.com>
2026-05-08 13:07:38 +0530

    fix: correct Intlayer metro plugin name

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro')
-const { withIntlayer } = require('react-native-intlayer/metro')
+const { configMetroIntlayerSync } = require('react-native-intlayer/metro')
 
 const config = getSentryExpoConfig(__dirname)
 
-module.exports = withIntlayer(config)
+module.exports = configMetroIntlayerSync(config)
